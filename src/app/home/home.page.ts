f12efd8717dbe1ccb4786fc0cce87aa6598a5d4d
Bruno Peçanha <brunoitdesenv@gmail.com>
2022-02-21 00:57:08 -0300

    🚧 Ajusta swiping;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, Renderer2, ViewChild } from '@angular/core';
-import { AnimationController, Animation, Platform } from '@ionic/angular';
+import { AnimationController, Animation, Platform, Gesture, GestureController, GestureDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,7 @@ export class HomePage  {
 
   @ViewChild('blocks') blocks: any;
   @ViewChild('background') background: any;
+  @ViewChild('swipeDown') swipeDown: any;
   public options: Array<any> = [
     { 
       icon: 'people', 
@@ -58,28 +59,77 @@ export class HomePage  {
   public initialStep: number = 0;
   private maxTranslate:  number;
   private animation: Animation;
+  private gesture: Gesture
+  public swiping: boolean = false;
 
   constructor(private animationCtrl: AnimationController,
     private plataform: Platform, 
-    private render: Renderer2
+    private render: Renderer2,
+    private gestureCtrl: GestureController
     ) 
     {
-      this.maxTranslate = this.plataform.height() - 100;
+      this.maxTranslate = this.plataform.height() - 180;
     }
 
     ngAfterViewInit(){   
       this.createAnimation();
+      this.detectSwipe();
+    }
+
+    detectSwipe() {
+      this.gesture = this.gestureCtrl.create(
+        {
+          el: this.swipeDown.el,
+          gestureName: 'swipe-down',
+          threshold: 0,
+          onMove: ev => this.onMove(ev),
+          onEnd: ev => this.onEnd(ev)
+        }, true);
+
+        this.gesture.enable(true);
+    }
+
+    onMove(ev: GestureDetail) {
+        if(!this.swiping){
+          this.animation.direction('normal').progressStart(true);
+          this.swiping = true;
+        }
+
+        const step: number = this.getStep(ev);
+        this.animation.progressStep(step);
+        this.setBackGroundOapacity(step);
+    }
+
+    getStep(ev: GestureDetail): number{
+      return (this.initialStep + ev.deltaY) / this.maxTranslate;
+    }
+    
+    onEnd(ev: GestureDetail) {
+      if (!this.swiping) return;
+
+      this.gesture.enable(false);
+      
+      const step: number = this.getStep(ev);
+      const shouldComplete: boolean = step > 0.5;
+
+      this.animation.progressEnd(shouldComplete ? 1 : 0, step);
+
+      this.initialStep = shouldComplete ? this.maxTranslate : 0;
+      this.setBackGroundOapacity();
+      this.swiping = false;
+
     }
 
    toggleBlocks() {     
      this.initialStep = this.initialStep === 0 ? this.maxTranslate : 0;
+     this.gesture.enable(false);
      this.animation.direction(this.initialStep === 0 ? 'reverse': 'normal').play();
 
      this.setBackGroundOapacity();
    } 
 
-  setBackGroundOapacity() {
-    this.render.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1');
+  setBackGroundOapacity(value: number = null) {
+    this.render.setStyle(this.background.nativeElement, 'opacity', value ? value: this.initialStep === 0 ? '0' : '1');
   }
 
   createAnimation() {    
@@ -87,5 +137,10 @@ export class HomePage  {
     .addElement(this.blocks.nativeElement)
     .duration(300)
     .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`)
+    .onFinish(() => this.gesture.enable(true));
+  }
+
+  fixedBlocks() : boolean {
+    return this.swiping || this.initialStep === this.maxTranslate;
   }
 }
